572f00a6054409adc03ed505677e0ba731f36908
Joseph Ojo <70476696+OjoFemiJoseph@users.noreply.github.com>
2024-08-25 07:39:33 +0100

    ff

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,11 @@
-# Use the official Python image from the Docker Hub
 FROM python:3.9-slim
 
-# Set the working directory
 WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
 
 COPY src/ .
 
-RUN pip install --no-cache-dir -r requirements.txt
-
-RUN apt-get update && \
-    apt-get install -y curl unzip && \
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
-    unzip awscliv2.zip && \
-    ./aws/install && \
-    rm -rf awscliv2.zip aws
-
-CMD ["fastapi", "run", "main.py", "--port", "80"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- src/main.py
+++ src/main.py
@@ -10,38 +10,7 @@ load_dotenv()
 
 app = FastAPI()
 
-sts_client = boto3.client('sts')
-
-response = sts_client.assume_role(
-    RoleArn='arn:aws:iam::635933673805:role/devops_role',
-    RoleSessionName='MySessionName'
-)
-
-credentials = response['Credentials']
-
-assumed_role_session = boto3.Session(
-    aws_access_key_id=credentials['AccessKeyId'],
-    aws_secret_access_key=credentials['SecretAccessKey'],
-    aws_session_token=credentials['SessionToken']
-)
-
-s3 = assumed_role_session.client('s3')
-
 @app.get('/')
 def index():
     return 200, "Hello World"
 
-@app.post("/upload_image")
-async def upload_image(file: UploadFile = File(...)):
-    bucket_name = os.environ['BUCKET_NAME']
-
-    try:
-        image_data = await file.read()
-        image_key = f"images/{file.filename}"
-
-        s3.put_object(Body=image_data, Bucket=bucket_name, Key=image_key, ContentType=file.content_type)
-
-        return JSONResponse(status_code=201, content={"message": "Image uploaded successfully"})
-    except Exception as e:
-        print(f"Error uploading image: {e}")
-        raise HTTPException(status_code=400, detail="Error uploading image")
